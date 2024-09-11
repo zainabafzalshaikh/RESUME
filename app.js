e5b8e7d3bdfecff5387a3a346e3bc4179f77f021
zainabafzalshaikh <161166927+zainabafzalshaikh@users.noreply.github.com>
2024-09-10 22:36:17 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var toggleSkillsButton = document.getElementById('toggle-skills');
+var skillsSection = document.getElementById('skills');
+toggleSkillsButton.addEventListener('click', function () {
+    if (skillsSection.style.display === 'none') {
+        skillsSection.style.display = 'block';
+    }
+    else {
+        skillsSection.style.display = 'none';
+    }
+});
